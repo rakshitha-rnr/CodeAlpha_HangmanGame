c81c346a55920103eb7cbf6daff3c4f38bb9bf33
T Rakshitha <tatigumtanikhitha@gmail.com>
2026-05-20 18:54:01 +0530

    hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -1,39 +1,66 @@
 import random
 
-words = ["cat", "dog", "sun"]
+# List of words
+words = ["python", "laptop", "guitar", "college", "student"]
 
+# Choose random word
 word = random.choice(words)
 
-guess = ""
-chance = 5
+# Variables
+guessed_letters = []
+wrong_guesses = 0
+max_guesses = 6
 
-print("🎮 Hangman Game")
+print("🎮 Welcome to Hangman Game")
+print("Guess the word letter by letter!")
 
-while chance > 0:
+# Game loop
+while wrong_guesses < max_guesses:
 
-    show = ""
+    display_word = ""
 
-    for i in word:
-        if i in guess:
-            show += i + " "
+    # Display guessed letters
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
         else:
-            show += "_ "
+            display_word += "_ "
 
-    print(show)
+    print("\nWord:", display_word)
 
-    if "_" not in show:
-        print("🎉 You Win")
+    # Check win
+    if "_" not in display_word:
+        print("🎉 Congratulations! You Won!")
+        print("The word was:", word)
         break
 
-    letter = input("Enter letter: ")
+    # User input
+    guess = input("Enter a letter: ").lower()
 
-    guess += letter
+    # Check valid input
+    if len(guess) != 1:
+        print("⚠ Enter only one letter.")
+        continue
 
-    if letter not in word:
-        chance -= 1
-        print("❌ Wrong")
-        print("Chances left:", chance)
+    # Check repeated guess
+    if guess in guessed_letters:
+        print("⚠ You already guessed this letter.")
+        continue
 
-if chance == 0:
-    print("💀 Game Over")
-    print("Word was:", word)
+    # Add guessed letter
+    guessed_letters.append(guess)
+
+    # Check correct or wrong
+    if guess in word:
+        print("✅ Correct Guess!")
+    else:
+        wrong_guesses += 1
+        print("❌ Wrong Guess!")
+        print("Remaining Chances:", max_guesses - wrong_guesses)
+
+# Lose condition
+if wrong_guesses == max_guesses:
+    print("\n💀 Game Over!")
+    print("Correct word was:", word)
+
+print("\nThanks for playing Hangman Game 🎮")
